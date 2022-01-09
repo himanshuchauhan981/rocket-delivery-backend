fb3a98a142561523d0a23059edf68c5eb4d581d8
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2022-01-09 21:40:10 +0530

    database configuration changed

--- src/core/database/database.module.ts
+++ src/core/database/database.module.ts
@@ -1,6 +1,19 @@
-import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 
-import { databaseProviders } from './database.provider';
+const DatabaseModule = [
+  SequelizeModule.forRootAsync({
+    useFactory: () => ({
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT, 10),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      dialect: 'postgres',
+      logging: true,
+      synchronize: true,
+      models: [],
+    }),
+  }),
+];
 
-@Module({ imports: [...databaseProviders] })
-export class DatabaseModule {}
+export default DatabaseModule;
