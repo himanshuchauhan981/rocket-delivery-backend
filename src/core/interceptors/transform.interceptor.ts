a13c7a6ddabe26bee6cace2397470ffa06918ac4
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2022-01-09 22:54:42 +0530

    response interceptor

---
+++ src/core/interceptors/transform.interceptor.ts
@@ -0,0 +1,33 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Response<T> {
+  statusCode: number;
+  message: number;
+  data: T;
+}
+
+@Injectable()
+export class TransformInterceptor<T> implements NestInterceptor<T, any> {
+  intercept(context: ExecutionContext, next: CallHandler): any {
+    const apiResponse = context.switchToHttp().getResponse();
+
+    return next.handle().pipe(
+      map((response) => {
+        return {
+          statusCode: apiResponse.statusCode,
+          message: response.message,
+          data: {
+            result: response.data,
+          },
+        };
+      }),
+    );
+  }
+}
