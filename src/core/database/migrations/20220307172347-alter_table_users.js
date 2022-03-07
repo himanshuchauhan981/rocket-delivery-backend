ddbcae7fc79fc65f1a23ceba0699847c29c560bb
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2022-03-08 00:07:35 +0530

    added is_active key in user table

---
+++ src/core/database/migrations/20220307172347-alter_table_users.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn(
+      'users',
+      'is_active', 
+      { type: Sequelize.BIGINT, defaultValue: 1 },
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
