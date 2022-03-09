84f606f507107a2c65e25e64f3a8de1ce7f192be
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2022-03-09 23:37:52 +0530

    user id type change

--- src/core/database/migrations/20220115084019-create_user.js
+++ src/core/database/migrations/20220115084019-create_user.js
@@ -13,7 +13,7 @@ module.exports = {
       'users',
       {
         id: {
-          type: Sequelize.BIGINT,
+          type: Sequelize.INTEGER,
           allowNull: false,
           autoIncrement: true,
           unique: true,
