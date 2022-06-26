591d62853c149be01d0f4372ac4650f0ede31874
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2022-06-26 21:01:54 +0530

    product description migration
    Signed-off-by: himanshuchauhan981 <himanshuchauhan981@gmail.com>

---
+++ src/core/database/migrations/20220626085918-alter_table_product.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('products', 'minumum_cart_quantity', {
+      type: Sequelize.INTEGER,
+    });
+
+    await queryInterface.addColumn('products', 'maximum_cart_quantity', {
+      type: Sequelize.INTEGER,
+    });
+
+    await queryInterface.addColumn('products', 'stock_visibility', {
+      type: Sequelize.ENUM,
+      values: ['STOCK_QUANTITY', 'STOCK_TEXT', 'HIDE_STOCK'],
+    });
+
+    await queryInterface.removeColumn('products', 'purchase_limit');
+
+    await queryInterface.addColumn('products', 'refundable', {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    });
+
+    // await queryInterface.removeColumn('products', 'description');
+
+    await queryInterface.addColumn('products', 'description_id', {
+      type: Sequelize.INTEGER,
+      unique: true,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.addColumn('products', 'purchase_limit', {
+      type: Sequelize.INTEGER,
+    });
+
+    await queryInterface.removeColumn('products', 'minumum_cart_quantity');
+
+    await queryInterface.removeColumn('products', 'maximum_cart_quantity');
+
+    await queryInterface.removeColumn('products', 'stock_visibility');
+
+    await queryInterface.removeColumn('products', 'description_id');
+
+    await queryInterface.removeColumn('products', 'refundable');
+
+    await queryInterface.removeColumn('products', 'description_id');
+
+    await queryInterface.addColumn('products', 'description', {
+      type: Sequelize.STRING,
+    });
+  },
+};
